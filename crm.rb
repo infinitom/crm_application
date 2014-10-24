4b8c787e7e950de78d45ea18ca607d1385173b4d
infinitom <deva.nadesan@infinitom.com>
2014-10-24 14:27:36 -0400

    Working crm start and menu display

--- crm.rb
+++ crm.rb
@@ -8,11 +8,25 @@ require_relative './contact.rb'
 require_relative './rolodex.rb'
 
 class CRM
-	def initialize
-		
+	
+	@@menu_items = {
+		"1" => "Add a contact",
+		"2" => "Modify a contact",
+		"3" => "Display all contacts",
+		"4" => "Display a contact",
+		"5" => "Display an attribute",
+		"6" => "Delete a contact",
+		"7" => "Exit"
+	}
+
+	def menu_maker
+		@@menu_items.each { |key, value| puts "[#{key}]\t#{value}"}
 	end
-	def to_s
-		"Welcome to BitMaker Labs CRM\n============================"
+
+
+	def start
+		puts "Welcome to BitMaker Labs CRM\n============================"
+		menu_maker
 	end
 end
 
@@ -20,4 +34,5 @@ end
 system 'clear'
 
 crm = CRM.new
-puts crm
+
+crm.start
